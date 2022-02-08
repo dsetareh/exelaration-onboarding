0b423cb12f3f47464ccfb2e24d81bbd0a2ee9b8a
dsetareh <dsetareh@vt.edu>
2022-02-08 10:54:55 -0500

    reqs #1 and #2 complete, starting #3

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var app;
 
 function getData() {
     fetch('https://xc-countries-api.herokuapp.com/api/countries/')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-        app.countries = data;
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            app.countries = data;
         }
-    )
+        )
 }
 
 
@@ -18,7 +18,24 @@ function startVue() {
         el: '#vuediv',
         data: {
             countries: [],
-            selected: '',
+            selectedCountry: '',
+            selectedState: '',
+            states: [],
+        },
+        watch: { // watch for changes in the selected country
+            selectedCountry: function (val) {
+                console.log(val);
+                // load states
+                fetch('https://xc-countries-api.herokuapp.com/api/countries/' + val + '/states/')
+                    .then((response) => {
+                        return response.json();
+                    }
+                    )
+                    .then((data) => {
+                        this.states = data;
+                    }
+                    )
+            }
         }
     });
 
